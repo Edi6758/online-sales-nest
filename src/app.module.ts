759c1532a88916993e94e2df539f5f86907a3e54
Edi6758 <edivaldo.souza@mathex.com.br>
2023-09-18 12:04:06 -0300

    feat: create conection from local database and create localdatabase from docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      envFilePath: ['env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
